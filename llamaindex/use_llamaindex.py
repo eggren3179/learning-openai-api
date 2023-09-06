69aa86544163940c14dc6e1436feb5c64d6fe437
eggren3179 <irteon@gmail.com>
2023-09-07 01:37:36 +0900

    LlamaIndexを使った簡易的なドキュメント読み込みと回答生成のスクリプト追加

---
+++ llamaindex/use_llamaindex.py
@@ -0,0 +1,63 @@
+import openai
+import os
+import sys
+from dotenv import load_dotenv
+import numpy as np
+import logging
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, StorageContext, load_index_from_storage
+
+#ログレベルの設定
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, force=True)
+
+#環境変数からOpenAIのAPIキー読み込み
+load_dotenv(override=True)
+openai.api_key = os.getenv("openai_api_key")
+
+def create_new_index(input_dir="./data", save_dir="./storage"):
+    """input_dirのディレクトリ内のデータを基にインデックスを作成し、save_dirのディレクトリ内に保存する
+    """
+    # ドキュメント読み込み
+    logging.info("----Start: loading documents-----")
+    documents = SimpleDirectoryReader(input_dir=input_dir).load_data()
+    logging.debug(f"documents: {documents}")
+    logging.info("----End: loading documents-----\n")
+
+    # インデックスの作成：時間がかかるので注意
+    logging.info("----Start: making index-----")
+    index = GPTVectorStoreIndex.from_documents(documents)
+    logging.info(f"index:{index}")
+    logging.info("----End: making index-----\n")
+
+    # インデックスの保存
+    logging.info("----Start: saving index-----")
+    index.storage_context.persist(persist_dir=save_dir)
+    logging.info("----End: saving index-----\n")
+
+    return index
+
+def send_query_and_response(query_engine, query):
+    response = query_engine.query(query)
+    print(f"query:{query}")
+    print(f"response:{response}")
+    return response
+
+
+# 保存したインデックスがある場合には読み込み。なければインデックスの新規作成を実行
+try:
+    logging.info("----Start: loading index-----")
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+    logging.info("----End: loading index-----\n")
+except Exception as e:
+     logging.info(f"Loading index failed.")
+     logging.info(f"Try to create index...")
+     index = create_new_index()
+
+# query engineの作成
+logging.info("----Start: making query engine-----")
+query_engine = index.as_query_engine()
+logging.info("----End: making query engine-----\n")
+
+# 質問応答
+query = "Azure OpenAI Serviceとは？"
+send_query_and_response(query_engine, query)
